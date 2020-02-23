d8ba793814e18008a32f7559a68cff6ade156de6
Roat Kim <roat95@hotmail.com>
2020-02-24 00:01:38 +0100

    First commit

---
+++ src/ihm/Main.java
@@ -0,0 +1,10 @@
+package ihm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Projet Faiz Faiz et Roro");
+	}
+
+}
