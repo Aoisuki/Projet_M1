3917b8f110b27b1ec2b0077aa34907ee436e4e6f
Roat Kim <Roat Kim@DESKTOP-E1QA4A7>
2020-02-24 00:17:56 +0100

    Second commit

--- src/ihm/Main.java
+++ src/ihm/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Projet Faiz Faiz et Roro");
+       System.out.println("Projet Faiz Faiz et Roro :) :p :3 ;) =D uWu (*.*) :o )");
 	}
 
 }
