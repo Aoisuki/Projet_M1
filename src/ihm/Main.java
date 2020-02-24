ade289f200ae62736f7b9030ddc9a3aa8906642f
faiza <faiza@192.168.56.1>
2020-02-24 13:13:17 +0100

    :)

--- src/ihm/Main.java
+++ src/ihm/Main.java
@@ -5,6 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Projet Faiz Faiz et Roro :) :p :3 ;) =D uWu (*.*) :o )");
+       int age_moyen;
+       char roat;
+       char faiza;
+       roat = 16;
+       faiza= 25;
+       age_moyen = (roat+faiza)/2;
+       {
+       System.out.println(age_moyen);
+       }
+    		   
 	}
 
 }
