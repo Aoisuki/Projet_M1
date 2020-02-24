78a2cddb6bdc8f1bb8fae7dd9ebfd7f773519db1
Roat Kim <roat95@hotmail.com>
2020-02-24 13:28:58 +0100

    ajouté une class personnage

---
+++ src/ihm/Personnage.java
@@ -0,0 +1,15 @@
+package ihm;
+
+public class Personnage {
+	private int hp;
+	private int mp;
+	private char pseudo;
+	private int exp;
+	private boolean enVie;
+	private int niveau;
+	private char sexe;
+	private int pointAttaque;
+	public Personnage() {
+		
+	}
+}
