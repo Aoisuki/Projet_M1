890e74404944e838e66a33cfe28ea5995427e422
Roat Kim <roat95@hotmail.com>
2020-02-24 13:22:46 +0100

    ajouté une classe

---
+++ src/ihm/Avion.java
@@ -0,0 +1,20 @@
+package ihm;
+
+import java.sql.Date;
+
+public class Avion {
+	private Date heure;
+	private int numero;
+	private int nombrePlaces;
+	private char nom_pilote[];
+	private int nombrePilote;
+	public Avion() {
+		
+	}
+	public void getNombrePlaces(){
+		System.out.println(nombrePlaces);
+	}
+	public void setNombrePlaces(int a) {
+		nombrePlaces = a; 
+	}
+}
