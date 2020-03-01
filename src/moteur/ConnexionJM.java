623ed399b4162ed55714d229cf3ad3bc746053ce
Roat Kim <Roat Kim@DESKTOP-E1QA4A7>
2020-03-01 19:39:06 +0100

    Add ConnexionJM class

---
+++ src/moteur/ConnexionJM.java
@@ -0,0 +1,45 @@
+package moteur;
+import java.sql.*;
+
+public class ConnexionJM {
+		
+		public static void main(String[] args) {
+			
+			try {
+			Connection c = connecterDB(); 
+			Statement st;
+			ResultSet rst;
+			
+			
+			st=c.createStatement();
+			rst=st.executeQuery("SELECT * FROM user");
+			
+				while(rst.next()) {
+					System.out.print(rst.getInt("id")+"\t");
+					System.out.print(rst.getString("nom")+"\t");
+					System.out.print(rst.getInt("age")+"\t");
+				}
+			
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		public static Connection connecterDB() {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				System.out.println("Driver marche !");
+				String url="jdbc:mysql://localhost/test";
+				String user="root";
+				String pw="";
+				
+				Connection c = DriverManager.getConnection(url,user,pw);
+				
+				System.out.println("Vous êtes connecter !");
+				return c;
+			}catch(Exception e) {
+				e.printStackTrace();
+				System.out.println("Oups... ça marche pas ! ");
+				return null;
+			}
+		}
+}
